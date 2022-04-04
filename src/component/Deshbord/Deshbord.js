40c8b4b2f80319195a44b4f03104915adefd78bf
Taposhpaul <taposhp05@gmail.com>
2022-04-04 21:29:10 +0600

    added line chart and css

--- src/component/Deshbord/Deshbord.js
+++ src/component/Deshbord/Deshbord.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Bar, BarChart, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import './Deshbord.css'
 
 const Deshbord = () => {
     const [data, setData] = useState([]);
@@ -20,17 +21,27 @@ const Deshbord = () => {
             })
 
     }, [])
-    // const { investment, month, revenue, sell } = data
     return (
         <div className='rechart-conteiner'>
             <div className="line-chart">
-
+                <LineChart width={630} height={250} data={data}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="revenue" stroke="#8884d8" />
+                    <Line type="monotone" dataKey="sell" stroke="#82ca9d" />
+                </LineChart>
             </div>
             <div className="bar-chart">
                 <BarChart width={550} height={440} data={data}>
                     <Bar dataKey="investment" fill="#8884d8" />
+                    <Bar dataKey="revenue" fill="#82ca9d" />
                     <XAxis dataKey="month" />
                     <YAxis />
+                    <Tooltip />
+                    <Legend />
                 </BarChart>
             </div>
         </div>
